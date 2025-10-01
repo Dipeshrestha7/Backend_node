eb4cc096d1fd40ce3ac6e5f87c23118e8d81fb35
Dipesh Shrestha <dipstha061@gmail.com>
2025-10-01 21:07:15 +0545

    express basic

--- 03_express/script.js
+++ 03_express/script.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express()
 
+///middlewear
+app.use((req, res, next)=>{
+    console.log("middlewear");
+    next()
+});
+
+///middlewear
+app.use((req, res, next)=>{
+    console.log("middlewearagina");
+    next()
+});
+
 app.get("/", (req, res) =>{
     res.send("This is Home Page");
 })
 
 app.get("/profile", (req, res) =>{
     res.send("This is Profile Page");
+})
+
+app.use((error, req, res, next)=>{
+    console.error(error.stack)
+    res.status(500).send("Something broke")
 })
 
 app.listen(3000)
