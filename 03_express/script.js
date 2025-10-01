d3e9579e032ffca227d8e44334da02a77a106a51
Dipesh Shrestha <dipstha061@gmail.com>
2025-10-01 15:44:25 +0545

    New folder express

---
+++ 03_express/script.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+
+app.get("/", (req, res) =>{
+    res.send("This is Home Page");
+})
+
+app.get("/profile", (req, res) =>{
+    res.send("This is Profile Page");
+})
+
+app.listen(3000)
